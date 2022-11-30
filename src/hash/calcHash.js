1c0664ed9cd480658b83d2ad26e5e12eb884af93
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-11-30 13:41:17 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { readFile } from "node:fs/promises";
+import { createHmac } from "node:crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const PATH = `${__dirname}\\files\\fileToCalculateHashFor.txt`;
+  const content = await readFile(PATH, { encoding: "utf-8" });
+  const hash = createHmac("sha256", content).digest("hex");
+
+  console.log(hash);
 };
 
 await calculateHash();
