aa96da5f2a89bd1cca394b9dc4dbcb9cdd96ab9a
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-12-01 17:21:43 +0300

    feat: implement streams tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const PATH = `${__dirname}\\files\\fileToWrite.txt`;
+  const ws = createWriteStream(PATH);
+
+  process.stdin.on("data", (data) => {
+    ws.write(data.toString());
+  });
 };
 
 await write();
